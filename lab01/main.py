f2f3a0dde594c8e69954059f89591c88e9d5ee9f
jantar <mikikubs@gmail.com>
2025-03-22 19:19:58 +0100

    feat: graph pickle

--- lab01/main.py
+++ lab01/main.py
@@ -1,39 +1,47 @@
 import pandas as pd
-import sys
 from dijkstra import dijkstra
 from a_star import a_star_search
+from tabu import tabu_search
 from models import Graph, CommunicationStep, Path, OptimizationCriterion
-    
-csv_filename = "connection_graph.csv"
+import pickle
+import os
+
+csv_filename = "connection_graph"
 separator = ","
 skipfooter = 0
 
-import pandas as pd
-
-df: pd.DataFrame
-try:
-    df = pd.read_csv(f"data/{csv_filename}", encoding="utf-8", sep=separator, skipfooter=skipfooter, engine="python")
-except FileNotFoundError:
-    df = pd.read_csv(f"lab01/data/{csv_filename}", encoding="utf-8", sep=separator, skipfooter=skipfooter, engine="python")
+if os.path.exists(f"data/{csv_filename}.pkl"):
+    with open(f"{csv_filename}.pkl", "rb") as f:
+        graph: Graph = pickle.load(f)
+    print("Graph loaded from graph.pkl.")
+else:
+    df: pd.DataFrame
+    try:
+        df = pd.read_csv(f"data/{csv_filename}.csv", encoding="utf-8", sep=separator, skipfooter=skipfooter, engine="python")
+    except FileNotFoundError:
+        df = pd.read_csv(f"lab01/data/{csv_filename}.csv", encoding="utf-8", sep=separator, skipfooter=skipfooter, engine="python")
     
-#df: pd.DataFrame = pd.read_csv("data/connection_graph.csv", dtype={"line": str})
+    #df: pd.DataFrame = pd.read_csv("data/connection_graph.csv", dtype={"line": str})
         
-graph = Graph()
+    graph = Graph()
 
-for _, row in df.iterrows():
-    step: CommunicationStep = CommunicationStep.from_parsed_csv_line(list(row))
+    for _, row in df.iterrows():
+        step: CommunicationStep = CommunicationStep.from_parsed_csv_line(list(row))
+        
+        for stop in [step.start_stop, step.end_stop]:
+            if stop.name not in graph.nodes:
+                graph.nodes[stop.name] = stop
+                graph.adjacency_list[stop.name] = set()
+        
+        key: tuple[str, str] = (step.start_stop.name, step.end_stop.name)
+        if step not in graph.edges[key]:
+            graph.edges[key].add(step)
+            graph.adjacency_list[step.start_stop.name].add(step)
+            
+    with open(f"data/{csv_filename}.pkl", "wb") as f:
+        pickle.dump(graph, f)
     
-    for stop in [step.start_stop, step.end_stop]:
-        if stop.name not in graph.nodes:
-            graph.nodes[stop.name] = stop
-            graph.adjacency_list[stop.name] = set()
-    
-    key: tuple[str, str] = (step.start_stop.name, step.end_stop.name)
-    if step not in graph.edges[key]:
-        graph.edges[key].add(step)
-        graph.adjacency_list[step.start_stop.name].add(step)
-    
-print("Done parsing .csv")
+    print("Done parsing .csv")
 
 r'''
 Wykorzystując udostępniony plik connection_graph.csv zaimplementuj
@@ -97,22 +105,8 @@ def algorithm_a_through_stops(a, stops, optimization_criterium, start_time) -> N
     path: Path
     
     try:
-        print("Dijkstra")        
-        path = dijkstra(a, b, start_time, graph)
-        path.pretty_print()
-    except TypeError as e:
-        print(e)
-
-    try:
-        print("Start A*")
-        path = a_star_search(a, b, start_time, graph, OptimizationCriterion.TIME)    
-        path.pretty_print()
-    except TypeError as e:
-        print(e)
-    
-    try:
-        print("Start A* 2")
-        path = a_star_search(a, b, start_time, graph, OptimizationCriterion.TRANSFERS)    
+        print("Tabu")        
+        path = tabu_search(a, stops, start_time, graph)
         path.pretty_print()
     except TypeError as e:
         print(e)
@@ -120,8 +114,8 @@ def algorithm_a_through_stops(a, stops, optimization_criterium, start_time) -> N
 
     
 a = "Prusa"
-b = "DWORZEC GŁÓWNY"
-#b = "GAJ"
+#b = "DWORZEC GŁÓWNY"
+b = "GAJ"
 optimization_criterium = "p"
 start_time = "08:00:00"
 
@@ -136,4 +130,5 @@ start_time = "08:00:00"
 # optimization_criterium = input("podaj kryterium optymalizacyjne: wartość t oznacza minimalizację czasu dojazdu, wartość p oznacza minimalizację liczby zmian linii")
 # start_time = input("czas pojawienia się na przystanku początkowym")
     
-algorithm_a_to_b(a, b, optimization_criterium, start_time)
+# algorithm_a_to_b(a, b, optimization_criterium, start_time)
+algorithm_a_through_stops(a, ["GAJ", "Zajezdnia Obornicka"], optimization_criterium, start_time)
